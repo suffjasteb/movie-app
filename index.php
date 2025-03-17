e695a618649b31dfbe380c61ab7dced285682b9e
suffjasteb <suffceein12@gmail.com>
2025-03-17 10:27:39 +0700

    Improve movie search functionality on the homepage

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+require_once "functions/api.php";
+require_once "includes/header.php";
+require_once "config.php";
+
+$movies = null; // Inisialisasi variabel $movies sebagai null
+$page = rand(1, 50);
+
+
+if (isset($_GET['search'])) { // Mengecek apakah ada pencarian yang dikirim via URL 
+    $movies = searchMovies($_GET['search']); // query = yang di search user
+}
+?>
+
+<h2 class="text-center">Search All Movies</h2>
+
+
+<!-- menampilkan hasil pencarian -->
+<div class="row">
+    <?php if ($movies && $movies["Response"] == "True") :?>
+        <?php foreach($movies["Search"] as $movie) : ?>
+            <div class="col-md-4">
+            <div class="card mb-3">
+
+            
+  <img class="card-img-top" src="<?= $movie['Poster'] ?>" alt="<?= $movie['Poster'] ?>">
+  <div class="card-body">
+    <h5 class="card-title"><?= $movie['Title'] ?></h5>
+    <p class="card-text"><?= $movie['Year'] ?></p>
+    <p class="card-text">Type: <?= $movie['Type'] ?></p>
+    <a href="detail.php?id=<?= $movie['imdbID']?>" class="btn btn-primary">Go details</a>
+  </div>
+  </div>
+</div>
+<?php endforeach; ?>
+<?php elseif ($movie) : ?> 
+    <h3 class="text-center text-danger" ><?= $movie['Error'] ?></h3>
+<?php endif; ?>
+</div>
+
+<?php require_once 'includes/footer.php' ?>

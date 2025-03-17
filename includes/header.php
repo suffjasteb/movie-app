f92fb84ff26514485e01afd0c5f1f181f13187c5
suffjasteb <suffceein12@gmail.com>
2025-03-17 10:26:57 +0700

    fix navbar

--- includes/header.php
+++ includes/header.php
@@ -10,23 +10,49 @@
     <title>Movie App PHP</title>
 </head>
 <body>
-    <!-- nav -->
-
       <!-- nav -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-    <a class="navbar-brand" href="">
-    <span>WPU MOVIE</span>
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-exp>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">NETFLIX</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-link active" href="#">Search Movie</a>
-    </div>
+<!-- Home -->
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <!-- contact -->
+      <li class="nav-item">
+        <a class="nav-link" href="https://wa.me/+6281252859954">Contact</a>
+      </li>
+      <!-- github -->
+      <li class="nav-item">
+        <a class="nav-link" href="https://github.com/suffjasteb?tab=overview&from=2025-03-01&to=2025-03-16">Github</a>
+      </li>
+      <!-- genre -->
+      <!-- <li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown
+  </a>
+  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</li> -->
+
+<!-- form -->
+    </ul>
+    <form class="form-inline my-2 my-lg-0" method="GET">
+   <div class="input-group mb-3">
+   <input type="text" class="form-control" placeholder="Seach All Movies" name="search" id="search">
+   <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+   </div>
+
+    </form>
   </div>
-    </div>
 </nav>
 
 
